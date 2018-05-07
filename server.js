100bc10c8100dcc9a0238b6cdadb6811736d537d
northPQ <colurfulcat13@gmail.com>
2018-05-07 17:08:34 +0800

    nodejs

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const app = express();
+var bodyParser = require('body-parser');
+var food = require('./food.json');
+const port = 10021;
+
+app.use(express.static(__dirname + '/public'));//upload static files
+
+app.use(bodyParser.json()); //to support JSON-encoded bodies
+app.use(bodyParser.urlencoded({extended : false})); //to support URL-encoded bodies
+
+app.post('/post_data',function(req,res){
+  var x=req.body.meal;
+  res.send('<h1>' + food[x] + '<h1>');
+});
+
+app.listen(port);

460757e29c91e1d5175a14331111e512647a3f61
northPQ <colurfulcat13@gmail.com>
2018-05-30 13:11:08 +0800

    no css

--- public/main.js
+++ public/main.js
@@ -10,13 +10,26 @@ $(document).ready(function(){
         _password: $("input[name='password']").val(),
     },
     function(data){
+      if(data)
+      window.location.replace("/");
     }
     );
   });
   
   $("#enroll").click(function(e){
-    e.preventDefault();
+    console.log("enroll");
+    $.post(
+    '/into_enroll',
+    function(data){
+    console.log("post");
+      //$("#center_frame").html(data);
+      window.location.replace("/enroll_1.html");
+      //$('#frame_js').remove();
+      //$('body').append("<script id='frame_js' src='enroll/enroll_1.js'></script>");
+    }
+    );
   });
+    
 
   function onSignIn(googleUser) {
     // Useful data for your client-side scripts:
@@ -41,4 +54,42 @@ $(document).ready(function(){
     console.log("User sign out");
     });
   }    
+////////////////other
+////////////////enroll_1
+  $('#enroll_1_next').click(function(e){  
+    e.preventDefault();
+    let _account = $("input[name='account_set']").val();
+    let _password = $("input[name='password_set']").val();
+    if(_password == $("input[name='password_confirm']").val()){    
+      $.post(
+      '/into_enroll_2',
+      {
+         _account: _account,
+         _password:_password,
+      }
+      ,
+      function(data){
+        window.location.replace("/enroll_2.html");
+       //$('#center_frame').html(data);
+       //$('#frame_js').attr("src","enroll_2.js")
+      }
+      );
+    }
+  });
+
+  ////////////////enroll_2
+  $('#enroll_2_next').click(function(e){  
+    e.preventDefault();
+    
+    $.post(
+    '/enroll_end',
+    function(data){
+      window.location.replace("/");
+     //$('#center_frame').html(data);
+     //$('#frame_js').attr("src","enroll_2.js")
+    }
+    );
+  });
+////////////////other
 });
+
--- server.js
+++ server.js
@@ -1,22 +1,67 @@
 const express = require('express');
 const app = express();
+var fs = require('fs');
 var client_id = require('./client_id.json');
 const {OAuth2Client} = require('google-auth-library');
 const client = new OAuth2Client(client_id.id +'.apps.googleusercontent.com');
+var mysql = require('mysql');
+var inf = require('./database_inf.json');
 var bodyParser = require('body-parser');
-var food = require('./food.json');
 const port = 10030;
 
+var connection = mysql.createConnection({
+  host:inf._host,
+  user:inf._user,
+  password:inf._password,
+  database:inf._user
+});
+app.use(express.static(__dirname + '/app_frame'));//upload static files
 app.use(express.static(__dirname + '/public'));//upload static files
 
 app.use(bodyParser.json()); //to support JSON-encoded bodies
 app.use(bodyParser.urlencoded({extended : false})); //to support URL-encoded bodies
 
+connection.connect(function(err){
+  if(err) throw err;
+  //connection.end();
+});
+
 app.post('/signin',function(req,res){
   var account=req.body._account;
   var password=req.body._password;
-  res.sendfile('./public/main_page.html');
-  console.log(account+password);
+  
+  var sql="SELECT * FROM user_test WHERE account = ?";
+  connection.query(sql,[account],function(err,result){
+    if(err) throw err;
+    console.log(result + "HIHIHI");
+    if(result != null) res.end("OK");
+  });
+  res.end();
+});
+app.post('/into_enroll',function(req,res){
+  //fs.readFile('public/enroll/enroll_1.html',function(err,data){
+  //if(err) throw err;
+  //res.send(data);
+  //});
+  res.send("OK");
+});
+app.post('/into_enroll_2',function(req,res){
+  var account=req.body._account;
+  var password=req.body._password;
+  var value=[account, password, 'null'];
+  var sql="INSERT INTO user_test (account, passwd, username) VALUES ?";
+  connection.query(sql,[[value]],function(err,result){
+    if(err) throw err;
+    console.log("insert");
+  });
+  //fs.readFile('public/enroll/enroll_2.html',function(err,data){
+  //if(err) throw err;
+  //res.send(data);
+  //});
+  res.send("OK");
+});
+app.post('/enroll_end',function(req,res){
+  res.send("OK");
 });
 
 app.post('/give_id',function(req,res){

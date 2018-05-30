e833b3a3f619e2ada928028da2a8dffa0569bf1a
Brad <Brad@BradsMacBook-Air-2.local>
2018-05-30 11:51:53 +0800

    static page

--- app_frame/js/auxcss.js
+++ app_frame/js/auxcss.js
@@ -9,16 +9,16 @@ $(window).resize( function() {
 function auxiliaryCSS() {
     // set x-center-aligned
     let x_center_aligned = $('.x-center-aligned').each( function() {
-        let obj_width = parseInt( $(this).css('width') );
-        let parent_width = parseInt($( $(this).parent() ).css('width'));
+        let obj_width = $(this).width();
+        let parent_width = $( $(this).parent() ).width();
         $(this).css('transform', 'translateX(' + ( parent_width -  obj_width)/2 + 'px)');
     });
     // END set x-center-aligned
 
     // set y-center-aligned
     let y_center_aligned = $('.y-center-aligned').each( function() {
-        let obj_height = parseInt( $(this).css('height') );
-        let parent_height = parseInt($( $(this).parent() ).css('height'));
+        let obj_height = $(this).height();
+        let parent_height = $( $(this).parent() ).height();
         $(this).css('transform', 'translateY(' + ( parent_height -  obj_height)/2 + 'px)');
     });
     // END set y-center-aligned
--- app_frame/js/control.js
+++ app_frame/js/control.js
@@ -1,16 +1,89 @@
-function prevScene() {
-    $('#content_prev').css({
-        'z-index': '-1001',
-        'transform': 'translate3d(0, 0, 0)'
-    });
-    $('#content_current').toggleClass('slideright');
-    setTimeout( function() {
-        $('#content_prev').css({
-            'z-index': '-1000'
-        });
-        $('#content_current').css({
-            'z-index': '-1000',
-            'transform': 'translate3d(100%, 0, 0)'
-        });
-    }, 300);
+
+// when iframe change to another page, this function show be run to show/hide controls
+var sw_table = {
+    // back, more, favorite, share, search, comment, showfooter
+    'Explore': [0, 0, 0, 0, 1, 1, 1],
+    'GoogleMaps': [1, 0, 0, 0, 0, 0, 0],
+    'MeetingInfo': [1, 0, 1, 1, 0, 0, 0],
+    'ChatRoom': [1, 1, 0, 0, 0, 0, 0]
+};
+function adjustUIControl() {
+    let sw = sw_table[ current_loaded_page ];
+    console.log(current_loaded_page, sw);
+    if( sw[0] == 1)
+        $('#back_btn').show();
+    else
+        $('#back_btn').hide();
+    if( sw[1] == 1)
+        $('#more_btn').show();
+    else
+        $('#more_btn').hide();
+    if( sw[2] == 1)
+        $('#favorite_btn').show();
+    else
+        $('#favorite_btn').hide();
+    if( sw[3] == 1)
+        $('#share_btn').show();
+    else
+        $('#share_btn').hide();
+    if( sw[4] == 1)
+        $('#search_btn').show();
+    else
+        $('#search_btn').hide();
+    if( sw[5] == 1)
+        $('#commment_btn').show();
+    else
+        $('#comment_btn').hide();
+    if( sw[6] == 1) {
+        $('#content_current').css('height', (screen.height - 54 - 54)+'px');
+        $('#footer').show();
+    }
+    else {
+        $('#content_current').css('height', (screen.height - 54)+'px');
+        $('#footer').hide();
+    }
+}
+
+$(document).ready(function() {
+    window.addEventListener('message', messageForward);
+})
+
+// Forward receive postMessage to corresponding handler function
+function messageForward(e) {
+
+    if( e.data.message_name == 'GoogleMapsSelectedPosition' ) {
+        delete e.data.message_name;
+        receivedGoogleMapsSelectedPosition( e.data )
+    }
+
+    if( e.data.message_name == 'JoinMeeting' ) {
+        delete e.data.message_name;
+        receivedJoinMeeting( e.data );
+    }
+
+    if( e.data.message_name == 'SelectMeeting' ) {
+        delete e.data.message_name;
+        receivedSelectMeeting( e.data );
+    }
+}
+
+function receivedGoogleMapsSelectedPosition(place) {
+    console.log(place);
+}
+
+function receivedJoinMeeting(meeting_info) {
+    console.log( meeting_info );
+    redirectPage('ChatRoom');
+}
+
+function receivedSelectMeeting(meeting_info) {
+    console.log(meeting_info);
+    redirectPage('MeetingInfo');
+}
+
+function redirectPage(page) {
+    current_loaded_page = page;
+    $('#content_loaded').attr('src', './inside_frame/' + page + '/' + page + '.html');
+    console.log(page);
+    adjustUIControl();
 }

93414a0e6ea2989c42f474845083069093e5f752
Brad <Brad@BradsMacBook-Air-2.local>
2018-06-22 22:14:03 +0800

    add caches

--- public/sw.js
+++ public/sw.js
@@ -16,3 +16,17 @@ self.addEventListener('install', function(event) {
 self.addEventListener('activate', function() {
     console.log("SW Activated");
 });
+
+self.addEventListener('fetch', function(event) {
+    event.respondWith(
+        caches.match(event.request)
+            .then( function(res) {
+                if(res) {
+                    return res;
+                } else {
+                    return fetch(event.request);
+                }
+
+            })
+    );
+});
